fd3e1f6587d662235497e6990ea9db54b8f9710b
alonewerwolf <andriy.zhigil@gmail.com>
2020-11-10 14:18:26 +0200

    initial

--- test.py
+++ test.py
@@ -1,34 +1,59 @@
 import csv
 import re
 
-my_str = 'Зміни у замовленні: SalalatKr ⇾ Кактус 0995405779 ⇾ Городоцька 315 :: До закладу 11:20 :: До клієнта 11:45 :: в дорозі '
+class Dataset:
 
+    def __init__(self, date, order_status, company, courier_name, courier_phone, client_address, to_restaurant, to_client, delivery_status):
+        self.date = date
+        self.order_status = order_status
+        self.company = company
+        self.courier_name = courier_name
+        self.courier_phone = courier_phone
+        self.client_address = client_address
+        self.to_restaurant = to_restaurant
+        self.to_client = to_client
+        self.delivery_status = delivery_status
+
+
+    def __repr__(self):
+        return f'{self.date, self.order_status, self.company, self.courier_name, self.courier_phone, self.client_address, self.to_restaurant, self.to_client, self.delivery_status}'
+
+my_str = """smdelivery, [Oct 31, 2020 at 2:49:37 PM]:
+Зміни у замовленні: Magic_Bowls ⇾ NazarKaiser 0931841898 ⇾ Городоцька 226а :: До закладу 14:55 :: До клієнта  :: зайнята
+
+smdelivery, [Oct 31, 2020 at 2:49:50 PM]:
+Зміни у замовленні: Magic_Bowls ⇾ NazarKaiser 0931841898 ⇾ Кульпарківська 226 Л, кв.17 :: До закладу 14:55 :: До клієнта  :: зайнята
+
+smdelivery, [Oct 31, 2020 at 2:51:23 PM]:
+Зміни у замовленні: Magic_Bowls ⇾ NazarKaiser 0931841898 ⇾ Кульпарківська 226 Л, кв.17 :: До закладу 15:05 :: До клієнта  :: зайнята
+
+smdelivery, [Oct 31, 2020 at 2:51:35 PM]:
+Зміни у замовленні: Magic_Bowls ⇾ NazarKaiser 0931841898 ⇾ Городоцька 226а :: До закладу 15:05 :: До клієнта  :: зайнята"""
 
 def converter(message: str):
-    res = re.findall(r'(.*): (.*) ⇾ (.*) (.*) ⇾ (.*) :: До закладу (.*) :: До клієнта (.*) :: (.*)', message)
-    if len(res) > 0:
-        res = res[0]
-    else:
-        res = None
+    res = re.findall(r'smdelivery, \[(.*)\]:\n(.*): (.*) ⇾ (.*) (.*) ⇾ (.*) :: До закладу (.*) :: До клієнта (.*) :: (.*)', message)
+    output_list = []
+    for line in res:
+        data = Dataset(*line)
+        output_list.append(data)
 
-    return res
+    return output_list
+
+
+headers = Dataset('Дата', 'Статус замовлення', 'Компанія' , 'Ім\'я кур\'єра', 'Номер кур\'єра', 'Адреса клієнта', 'До закладу', 'До клієнта', 'Статус доставки')
 
 
 def csv_writer():
     message = converter(my_str)
     with open('message.csv', 'w') as file_csv:
-        field_name = ['Статус замовлення', 'Компанія', 'Імʼя курʼєра', 'Номер курʼєра', 'Адрес клієнта', 'До закладу',
-                      'До клієнта', 'Статус доставки']
-        writer = csv.DictWriter(file_csv, fieldnames=field_name)
+        f = (headers.date, headers.order_status, headers.company, headers.courier_name, headers.courier_phone, headers.client_address, headers.to_restaurant, headers.to_client, headers.delivery_status)
+        writer = csv.DictWriter(file_csv, fieldnames=f)
         writer.writeheader()
-        writer.writerow({'Статус замовлення': f'{message[0]}',
-                         'Компанія': f'{message[1]}',
-                         'Імʼя курʼєра': f'{message[2]}',
-                         'Номер курʼєра': f'{message[3]}',
-                         'Адрес клієнта': f'{message[4]}',
-                         'До закладу': f'{message[5]}',
-                         'До клієнта': f'{message[6]}',
-                         'Статус доставки': f'{message[7]}'})
+        for line in message:
+            file_csv.write(str(line))
 
 
 csv_writer()
+
+
+
